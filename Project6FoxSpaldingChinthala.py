9a51b2b38528ccf9e675e962e16c680ee7f5d96c
aaronfox <aaron.m.fox3@gmail.com>
2019-11-05 22:32:17 -0500

    Working on creating intial chromosomes in first population

--- Project6FoxSpaldingChinthala.py
+++ Project6FoxSpaldingChinthala.py
@@ -3,10 +3,33 @@
 # CECS 545-01
 # Dr. Yampolskiy
 
-class genetic_pancake_algorithm:
-    def __init__(self, original_unordered_string):
-        self.original_unordered_string = original_unordered_string
+import math # Need math for math.ceil for indices size (ceil)
 
+class genetic_pancake_algorithm:
+    def __init__(self, original_unordered_string, population_size):
+        self.original_unordered_string = original_unordered_string
+        self.initial_population = []
+        # self.
+
+        # Max number of indices required is found by 18*n / 11 according to Chittri 
+        indices_chromosome_size = math.ceil(18 * len(self.original_unordered_string) / 11)
+        print("indices_chromosome_size == " + str(indices_chromosome_size))
+
+        # For initial population, place every possible index to flip for the first n, then fill remaining
+        # spots with any number within the bounds of indices
+
+
+        # Generate list of all possible indices that can be included in chromosome
+        self.all_possible_indices = list(range(0, len(self.original_unordered_string)))
+
+        # Create number of initial populations as specified by population_size
+        for i in range(population_size):
+            self.
+
+
+# Reads in .string files made to be sorted using the pancake sorting algorithm
+# INPUT: a filepath to the location of the string file
+# OUTPUT: a char array of each letter of the strings
 def read_string(string_filepath):
     string_file = open(string_filepath, 'r')
     string_array = []
@@ -23,7 +46,7 @@ if __name__ == "__main__":
 
     print("string_array == " + str(string_array))
 
-    ga = genetic_pancake_algorithm(string_array)
+    ga = genetic_pancake_algorithm(string_array, 3)
 
 
 
