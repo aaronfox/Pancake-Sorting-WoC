46b0577d0f7508f0421ecfd89bef0cc34812ec0b
aaronfox <aaron.m.fox3@gmail.com>
2019-11-08 00:18:33 -0500

    Improved finding all subarrays

--- Project6FoxSpaldingChinthala.py
+++ Project6FoxSpaldingChinthala.py
@@ -110,8 +110,10 @@ class genetic_pancake_algorithm:
         
         # TODO: REMOVE THIS it's for testing
         string_with_value_dict = {'a': 0, 'f': 1, 'j': 2, 'z': 3}
+        # string_with_value_dict = {'1': 0, '2': 1, '3': 2, '4': 3}
         chromosome_string = ['a', 'f', 'j', 'a', 'f', 'j', 'z', 'a', 'f', 'z', 'j', 'a', 'a']
-        # chromosome_string = ['a', 'f', 'j', 'a', 'f']# 'j', 'z', 'a', 'f', 'z', 'j', 'a', 'a']
+        chromosome_string = ['a', 'f', 'j', 'f', 'a', 'a', 'f', 'a']# 'j', 'z', 'a', 'f', 'z', 'j', 'a', 'a']
+        # chromosome_string = ['2', '1', '1', '1', '2', '2', '4', '4']
 
         print("\n\n=================\n\nNEW string_with_value_dict == " + str(string_with_value_dict))
         print("NEW chromosome_string == " + str(chromosome_string))
@@ -136,8 +138,17 @@ class genetic_pancake_algorithm:
         if current_subarray_length > 1:
             if current_subarray_length not in subarray_and_occurences_dict:
                 subarray_and_occurences_dict[current_subarray_length] = 1
+                print("!!!AT END Updating subarray_and_occurences_dict to one! " + str(subarray_and_occurences_dict))
             else:
                 subarray_and_occurences_dict[current_subarray_length] = subarray_and_occurences_dict[current_subarray_length] + 1
+                print("!!!AT END Updating subarray_and_occurences_dict to more than one! " + str(subarray_and_occurences_dict))
+
+        # Reset current_subarray_length for second while loop
+        current_subarray_length = 1
+
+        # Check for same letter too to avoid counting it twice
+        current_subarray_length_same_letter = 1
+        number_of_same_letters = 0
 
         # Second while loop: just check for subarrays like ['z', 'j'] above (NOT for ['a', 'f'] and ['a', 'a'] yet)
         index = len(chromosome_string) - 1
@@ -154,28 +165,44 @@ class genetic_pancake_algorithm:
 
             if string_with_value_dict[chromosome_string[index]] + 1 == string_with_value_dict[chromosome_string[index - 1]]:
                 current_subarray_length = current_subarray_length + 1
-                print("incrementing current_subarray_length to " +
-                      str(current_subarray_length))
+                # current_subarray_length_same_letter = 1
+                print("incrementing current_subarray_length to " + str(current_subarray_length))
+            # Account for subarrays of same letters to add in with overall subarray, but don't add to main subarray length on its own like in first while
+            # loop to avoid counting for double array twice
+            if string_with_value_dict[chromosome_string[index]] == string_with_value_dict[chromosome_string[index - 1]]:
+                current_subarray_length_same_letter = current_subarray_length_same_letter + 1
+                print("incrementing SAME LETTER")
             else:
                 if current_subarray_length > 1:
-                    if current_subarray_length not in subarray_and_occurences_dict:
-                        subarray_and_occurences_dict[current_subarray_length] = 1
-                        print("!!!Updating subarray_and_occurences_dict to 1! " +
-                              str(subarray_and_occurences_dict))
+                    if current_subarray_length_same_letter > 1:
+                        print("IN SAME LETTER LENGTH")
+                        if current_subarray_length + current_subarray_length_same_letter - 1 not in subarray_and_occurences_dict:
+                            subarray_and_occurences_dict[current_subarray_length + current_subarray_length_same_letter - 1] = 1
+                            print("!!!REVERSE Updating subarray_and_occurences_dict to 1! " + str(subarray_and_occurences_dict))
+                        else:
+                            subarray_and_occurences_dict[current_subarray_length + current_subarray_length_same_letter - 1] = subarray_and_occurences_dict[current_subarray_length+ current_subarray_length_same_letter - 1] + 1
+                            print("!!!REVERSE Updating subarray_and_occurences_dict to more than one! " + str(subarray_and_occurences_dict))
                     else:
-                        subarray_and_occurences_dict[current_subarray_length] = subarray_and_occurences_dict[current_subarray_length] + 1
-                        print("!!!Updating subarray_and_occurences_dict to more than one! " +
-                              str(subarray_and_occurences_dict))
+                        if current_subarray_length not in subarray_and_occurences_dict:
+                            subarray_and_occurences_dict[current_subarray_length] = 1
+                        else:
+                            subarray_and_occurences_dict[current_subarray_length] = subarray_and_occurences_dict[current_subarray_length] + 1
 
                 current_subarray_length = 1
+                current_subarray_length_same_letter = 1
+
             index = index - 1
 
         # Account for if current subarray was at end
         if current_subarray_length > 1:
             if current_subarray_length not in subarray_and_occurences_dict:
                 subarray_and_occurences_dict[current_subarray_length] = 1
+                print("!!!AT END REVERSE Updating subarray_and_occurences_dict to one! " + str(subarray_and_occurences_dict))
+
             else:
                 subarray_and_occurences_dict[current_subarray_length] = subarray_and_occurences_dict[current_subarray_length] + 1
+                print("!!!AT END REVERSE Updating subarray_and_occurences_dict to more than one! " + str(subarray_and_occurences_dict))
+
 
         print("subarray_and_occurences_dict == " +
               str(subarray_and_occurences_dict))
@@ -200,7 +227,7 @@ if __name__ == "__main__":
 
     print("string_array == " + str(string_array))
 
-    ga = genetic_pancake_algorithm(string_array, 3)
+    ga = genetic_pancake_algorithm(original_unordered_string=string_array, population_size=1)
 
 
 
